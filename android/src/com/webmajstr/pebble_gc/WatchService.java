b66a3757b13cd2c4e68722ae08ae862797a1acd2
WebMajstr <nace@hudobivnik.si>
2015-01-06 16:59:28 +0100

    Updated the code. Implementation of compass in Pebble by Jason Brand

--- android/src/com/webmajstr/pebble_gc/WatchService.java
+++ android/src/com/webmajstr/pebble_gc/WatchService.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import com.getpebble.android.kit.PebbleKit;
 import com.getpebble.android.kit.util.PebbleDictionary;
 
-
 import android.app.Notification;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -13,12 +12,14 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.hardware.GeomagneticField;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 import android.widget.Toast;
 
 public class WatchService extends Service {
@@ -33,6 +34,8 @@ public class WatchService extends Service {
     float gc_difficulty, gc_terrain;
 	String gc_name, gc_code, gc_size;
 	
+	float declination = 1000;
+	
 	private static final int DISTANCE_KEY = 0;
 	private static final int BEARING_INDEX_KEY = 1;
 	private static final int EXTRAS_KEY = 2;
@@ -41,6 +44,7 @@ public class WatchService extends Service {
 	private static final int GC_CODE_KEY = 5;
 	private static final int GC_SIZE_KEY = 6;
 	private static final int AZIMUTH_KEY = 7;
+	private static final int DECLINATION_KEY = 8;
 	
     private UUID uuid = UUID.fromString("6191ad65-6cb1-404f-bccc-2446654c20ab"); //v2
     
@@ -58,7 +62,9 @@ public class WatchService extends Service {
 		registerReceiver(mReceiver, filter);
     	
     	//set to run in foreground, so it's not killed by android
-		showNotification();		
+		showNotification();	
+	
+		
 					
 		//register for GPS location updates
 		registerLocationUpdates();
@@ -111,7 +117,13 @@ public class WatchService extends Service {
 
     	if(!currentLocation.getProvider().equals("gps")) return;
     	
-    	
+    	// calculate declination at this point. This is done only once as it shouldn't change so much at similar location on earth ;)
+    	//only run this if first time!
+    	if (declination > 999){
+    		GeomagneticField geomagneticField = new GeomagneticField((float)currentLocation.getLatitude(),
+                (float)currentLocation.getLongitude(), (float)currentLocation.getAltitude(),currentLocation.getTime());
+    		declination = geomagneticField.getDeclination();
+    	}
     	float distance = currentLocation.distanceTo(geocacheLocation);
     	float deviceBearing = currentLocation.getBearing();
     	float azimuth = currentLocation.bearingTo(geocacheLocation);
@@ -143,12 +155,12 @@ public class WatchService extends Service {
     	// convert bearing in degrees to index of image to show. north +- 15 degrees is index 0,
     	// bearing of 30 degrees +- 15 degrees is index 1, etc..
     	int bearingIndex = ((bearingInt + 15)/30) % 12;
-    	
-    	sendToPebble(String.format("%d m", distanceInt), bearingIndex, azimuthInt);
+    	    	
+    	sendToPebble(String.format("%d m", distanceInt), bearingIndex, azimuthInt, Math.round(declination) );
     	
     }
     
-    public void sendToPebble(String distance, int bearingIndex, int azimuth) {
+    public void sendToPebble(String distance, int bearingIndex, int azimuth, int decl) {
     	
     	boolean hasExtras = checkHasExtras();
     	
@@ -157,7 +169,10 @@ public class WatchService extends Service {
         data.addString(DISTANCE_KEY, distance);
         data.addUint8(BEARING_INDEX_KEY, (byte)bearingIndex);
         data.addUint16(AZIMUTH_KEY, (short) azimuth);
+        data.addInt16(DECLINATION_KEY, (short) decl);
         data.addUint8(EXTRAS_KEY, (byte)(hasExtras?1:0) );
+
+        Log.i("Nace", String.valueOf( (short)decl ));
         
         if(hasExtras){
         	
@@ -221,7 +236,7 @@ public class WatchService extends Service {
                 
         //reset watch to default state
         //TODO do make sure the watchapp is listening to messages
-    	sendToPebble("NO GPS", 0, 0);
+    	sendToPebble("NO GPS", 0, 0, 0);
     	
     	Toast.makeText(this, R.string.navigation_has_started, Toast.LENGTH_LONG).show();
                 
--- pebble/src/geocaching.c
+++ pebble/src/geocaching.c
@@ -1,34 +1,65 @@
 #include "pebble.h"
 
-static const uint32_t COMPASS_ICONS[] = {
-  RESOURCE_ID_DIRECTION_0,
-  RESOURCE_ID_DIRECTION_1,
-  RESOURCE_ID_DIRECTION_2,
-  RESOURCE_ID_DIRECTION_3,
-  RESOURCE_ID_DIRECTION_4,
-  RESOURCE_ID_DIRECTION_5,
-  RESOURCE_ID_DIRECTION_6,
-  RESOURCE_ID_DIRECTION_7,
-  RESOURCE_ID_DIRECTION_8,
-  RESOURCE_ID_DIRECTION_9,
-  RESOURCE_ID_DIRECTION_10,
-  RESOURCE_ID_DIRECTION_11
+// ADDED GPath point definitions for arrow and north marker
+static const GPathInfo ARROW_POINTS =
+{
+  14,
+  (GPoint []) {
+    {0, -30},
+    {10, -20},
+    {6, -16},
+    {3, -20},
+    {3, 10},
+    {7, 15},
+    {7, 30},
+    {0, 22},
+    {-7, 30},
+    {-7, 15},
+    {-3, 10},
+    {-3, -20},
+    {-6, -16},
+    {-10, -20},
+  }
+};
+static const GPathInfo NORTH_POINTS =
+{
+  3,
+  (GPoint []) {
+    {0, -40},
+    {6, -31},
+    {-6, -31},
+  }
 };
 
+// updated to match new Adroid app
 enum GeoKey {
   DISTANCE_KEY = 0x0,
-  AZIMUT_INDEX_KEY = 0x1,
+  BEARING_INDEX_KEY = 0x1,
   EXTRAS_KEY = 0x2,
   DT_RATING_KEY = 0x3,
   GC_NAME_KEY = 0x4,
   GC_CODE_KEY = 0x5,
-  GC_SIZE_KEY = 0x6
+  GC_SIZE_KEY = 0x6,
+  AZIMUTH_KEY = 0x7,
+  DECLINATION_KEY = 0x8,
 };
 
 Window *window;
 
-BitmapLayer *image_layer;
-GBitmap *image_bitmap = NULL;
+// ADDED GPath definitions
+Layer *arrow_layer;
+GPath *arrow;
+GPath *north;
+GRect arrow_bounds;
+GPoint arrow_centre;
+
+int16_t compass_heading = 0;
+int16_t north_heading = 0;
+int16_t bearing = 0;
+uint8_t status;
+bool rot_arrow = false;
+bool gotdecl = false;
+int16_t declination = 0;
 
 TextLayer *text_distance_layer;
 TextLayer *text_time_layer;
@@ -50,18 +81,20 @@ static void sync_tuple_changed_callback(const uint32_t key,
 
     case DISTANCE_KEY:
       text_layer_set_text(text_distance_layer, new_tuple->value->cstring);
+      rot_arrow = (strncmp(text_layer_get_text(text_distance_layer), "NO", 2) != 0) ? true : false;
       break;
 
-    case AZIMUT_INDEX_KEY:
-      if (image_bitmap) {
-        gbitmap_destroy(image_bitmap);
-      }
-
-      image_bitmap = gbitmap_create_with_resource(
-          COMPASS_ICONS[new_tuple->value->uint8]);
-      bitmap_layer_set_bitmap(image_layer, image_bitmap);
+// NEW
+    case AZIMUTH_KEY:
+      bearing = new_tuple->value->int16;
+      layer_mark_dirty(arrow_layer);
       break;
 
+    case DECLINATION_KEY:
+      declination = new_tuple->value->int16;
+      gotdecl = true;
+      layer_mark_dirty(arrow_layer);
+      break;
   }
 }
 
@@ -71,20 +104,88 @@ void line_layer_update_callback(Layer *layer, GContext* ctx) {
   graphics_fill_rect(ctx, layer_get_bounds(layer), 0, GCornerNone);
 }
 
+// NEW compass handler
+void handle_compass(CompassHeadingData heading_data){
+/*
+  static char buf[64];
+  snprintf(buf, sizeof(buf), " %d°", declination);
+  text_layer_set_text(text_distance_layer, buf);
+*/
+  north_heading = TRIGANGLE_TO_DEG(heading_data.magnetic_heading) - declination;
+  if (north_heading >= 360) north_heading -= 360;
+  if (north_heading < 0) north_heading += 360;
+  status = heading_data.compass_status;
+  layer_mark_dirty(arrow_layer);
+}
+
+// NEW arrow layer update handler
+void arrow_layer_update_callback(Layer *path, GContext *ctx) {
+
+  graphics_context_set_fill_color(ctx, GColorWhite);
+  graphics_context_set_stroke_color(ctx, GColorWhite);
+
+  compass_heading = bearing + north_heading;
+  if (compass_heading >= 360) {
+    compass_heading = compass_heading - 360;
+  }
+
+// Don't rotate the arrow if we have NO GPS or NO BT in the distance box
+  if (rot_arrow) {
+    gpath_rotate_to(arrow, compass_heading * TRIG_MAX_ANGLE / 360);
+  }
+
+// draw outline arrow if pebble compass is invalid, solid if OK
+  if (status == 0) {
+    gpath_draw_outline(ctx, arrow);
+  } else {
+    gpath_draw_filled(ctx, arrow);
+  }
+
+// draw outline north arrow if pebble compass is fine calibrating, solid if good
+  gpath_rotate_to(north, north_heading * TRIG_MAX_ANGLE / 360);
+  if (!gotdecl) {
+    gpath_draw_outline(ctx, north);
+  } else {
+    gpath_draw_filled(ctx, north);
+  }
+
+/*
+// NEW draw a dot at North heading. This has been replaced by an arrow (looks better)
+
+  int north_dot_size = 5;
+  int north_length;
+
+  north_length = (arrow_centre.x < arrow_centre.y) ? arrow_centre.x : arrow_centre.y;
+  north_length -= north_dot_size / 2;
+
+  int north_x = (int16_t)(sin_lookup(north_heading * TRIG_MAX_ANGLE / 360) * north_length / TRIG_MAX_RATIO) + arrow_centre.x;
+  int north_y = (int16_t)(-cos_lookup(north_heading * TRIG_MAX_ANGLE / 360) * north_length / TRIG_MAX_RATIO) + arrow_centre.y;
+
+  graphics_fill_circle(ctx, GPoint(north_x, north_y), north_dot_size);
+  graphics_context_set_stroke_color(ctx, GColorBlack);
+  graphics_draw_circle(ctx, GPoint(north_x, north_y),north_dot_size);
+*/
+}
+
 void bluetooth_connection_changed(bool connected) {
 
   if (!connected) {
-    
-    if (image_bitmap) {
-      gbitmap_destroy(image_bitmap);
-    }
+// deleted bitmap destroy and redraw
 
-    image_bitmap = gbitmap_create_with_resource(RESOURCE_ID_NO_BT);
-    bitmap_layer_set_bitmap(image_layer, image_bitmap);
-    
+// ADDED text instead of bitmap
+    text_layer_set_text(text_distance_layer, "NO BT");
+
     //vibes_short_pulse();
+
+  } else {
+    const Tuple *tuple = app_sync_get(&sync, DISTANCE_KEY);
+    text_layer_set_text(text_distance_layer, (tuple == NULL) ? "NO GPS" : tuple->value->cstring);
   }
-  
+  if (strncmp(text_layer_get_text(text_distance_layer), "NO", 2) != 0) {
+    rot_arrow = true;
+  } else {
+    rot_arrow = false;
+  }
 }
 
 bool have_additional_data(){
@@ -92,103 +193,100 @@ bool have_additional_data(){
   return (tuple == NULL || tuple->value->uint8 == 0)? false : true;
 }
 
+void up_click_handler(ClickRecognizerRef recognizer, void *context) {
 
- void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  data_display++;
+  data_display %= 5;
 
-    data_display++;
-    data_display %= 5;
+  if ( !have_additional_data() ) data_display = 0;
 
-    if ( !have_additional_data() ) data_display = 0;
+  if(data_display == 0){
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+  } else if(data_display == 1){
 
-    if(data_display == 0){
-      tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
-      
-    } else if(data_display == 1){
+    tick_timer_service_unsubscribe();
+    const Tuple *tuple = app_sync_get(&sync, GC_NAME_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      tick_timer_service_unsubscribe();
-      const Tuple *tuple = app_sync_get(&sync, GC_NAME_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 2){
 
-    } else if(data_display == 2){
+    const Tuple *tuple = app_sync_get(&sync, GC_CODE_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      const Tuple *tuple = app_sync_get(&sync, GC_CODE_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 3){
 
-    } else if(data_display == 3){
+    const Tuple *tuple = app_sync_get(&sync, GC_SIZE_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      const Tuple *tuple = app_sync_get(&sync, GC_SIZE_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 4){
 
-    } else if(data_display == 4){
+    const Tuple *tuple = app_sync_get(&sync, DT_RATING_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      const Tuple *tuple = app_sync_get(&sync, DT_RATING_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  }
 
-    }
+}
 
-    
- }
+void down_click_handler(ClickRecognizerRef recognizer, void *context) {
 
- void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-    
-    if(data_display == 0) data_display = 5;
-    data_display--;
+  if(data_display == 0) data_display = 5;
+  data_display--;
 
-    if ( !have_additional_data() ) data_display = 0;
+  if ( !have_additional_data() ) data_display = 0;
 
-    if(data_display == 0){
-      tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
-    
-    } else if(data_display == 1){
+  if(data_display == 0){
+    tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
 
-      const Tuple *tuple = app_sync_get(&sync, GC_NAME_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 1){
 
-    } else if(data_display == 2){
+    const Tuple *tuple = app_sync_get(&sync, GC_NAME_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      const Tuple *tuple = app_sync_get(&sync, GC_CODE_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 2){
 
-    } else if(data_display == 3){
+    const Tuple *tuple = app_sync_get(&sync, GC_CODE_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      const Tuple *tuple = app_sync_get(&sync, GC_SIZE_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+  } else if(data_display == 3){
 
-    } else if(data_display == 4){
+    const Tuple *tuple = app_sync_get(&sync, GC_SIZE_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
 
-      tick_timer_service_unsubscribe();
+  } else if(data_display == 4){
 
-      const Tuple *tuple = app_sync_get(&sync, DT_RATING_KEY);
-      const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
-      text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+    tick_timer_service_unsubscribe();
 
-    }
-    
-    
- }
+    const Tuple *tuple = app_sync_get(&sync, DT_RATING_KEY);
+    const char *gc_data = tuple == NULL ? "" : tuple->value->cstring;
+    text_layer_set_text(text_time_layer, *gc_data ? gc_data : "??");
+
+  }
+
+}
 
 void config_buttons_provider(void *context) {
    window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
    window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
  }
 
-
 void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
-  static char time_text[] = "00:00";
+// UPDATED to include date
+  static char time_text[] = "XXX XX 00:00";
 
   char *time_format;
 
+// UPDATED to include date
   if (clock_is_24h_style()) {
-    time_format = "%R";
+    time_format = "%b %e %R";
   } else {
-    time_format = "%I:%M";
+    time_format = "%b %e %I:%M";
   }
 
   strftime(time_text, sizeof(time_text), time_format, tick_time);
@@ -202,7 +300,7 @@ void handle_minute_tick(struct tm *tick_time, TimeUnits units_changed) {
 
 void handle_init(void) {
   window = window_create();
-  
+
   window_set_background_color(window, GColorBlack);
 
   window_set_fullscreen(window, true);
@@ -210,27 +308,20 @@ void handle_init(void) {
 
   Layer *window_layer = window_get_root_layer(window);
 
-  // Initialize compass layout
-  Layer *compass_holder = layer_create(GRect(0, 0, 144, 79));
-  layer_add_child(window_layer, compass_holder);
-
-  image_layer = bitmap_layer_create(GRect(42, 15, 60, 60));
-  layer_add_child(compass_holder, bitmap_layer_get_layer(image_layer));
-
   // Initialize distance layout
   Layer *distance_holder = layer_create(GRect(0, 80, 144, 40));
   layer_add_child(window_layer, distance_holder);
 
   ResHandle roboto_36 = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_CONDENSED_36);
-  text_distance_layer = text_layer_create(GRect(8, 0, 144-8, 40));
+  text_distance_layer = text_layer_create(GRect(0, 0, 144, 40));
   text_layer_set_text_color(text_distance_layer, GColorWhite);
   text_layer_set_text_alignment(text_distance_layer, GTextAlignmentCenter);
   text_layer_set_background_color(text_distance_layer, GColorClear);
   text_layer_set_font(text_distance_layer, fonts_load_custom_font(roboto_36));
   layer_add_child(distance_holder, text_layer_get_layer(text_distance_layer));
 
-
   // Initialize time layout
+// Size adjusted so the date and time will fit
   Layer *date_holder = layer_create(GRect(0, 132, 144, 36));
   layer_add_child(window_layer, date_holder);
 
@@ -239,26 +330,49 @@ void handle_init(void) {
   layer_add_child(date_holder, line_layer);
 
   ResHandle roboto_22 = resource_get_handle(RESOURCE_ID_FONT_ROBOTO_BOLD_SUBSET_22);
-  text_time_layer = text_layer_create(GRect(16, 2, 144-32, 32));
+  text_time_layer = text_layer_create(GRect(0, 2, 144, 32));
   text_layer_set_text_color(text_time_layer, GColorWhite);
   text_layer_set_text_alignment(text_time_layer, GTextAlignmentCenter);
   text_layer_set_background_color(text_time_layer, GColorClear);
   text_layer_set_font(text_time_layer, fonts_load_custom_font(roboto_22));
   layer_add_child(date_holder, text_layer_get_layer(text_time_layer));
 
-  const int inbound_size = 150;
-  const int outbound_size = 0;
-  app_message_open(inbound_size, outbound_size);
+  // Initialize compass layout
+  Layer *compass_holder = layer_create(GRect(0, 0, 144, 79));
+  layer_add_child(window_layer, compass_holder);
+
+// deleted bitmap layer create
+
+// NEW definitions for Path layer, adjusted size due to a glitch drawing the North dot
+  arrow_layer = layer_create(GRect(0, 0, 144, 79));
+  layer_set_update_proc(arrow_layer, arrow_layer_update_callback);
+  layer_add_child(compass_holder, arrow_layer);
+
+// centrepoint added for use in rotations
+  arrow_bounds = layer_get_frame(arrow_layer);
+  arrow_centre = GPoint(arrow_bounds.size.w/2, arrow_bounds.size.h/2);
+
+// Initialize and define the paths
+  arrow = gpath_create(&ARROW_POINTS);
+  gpath_move_to(arrow, arrow_centre);
+  north = gpath_create(&NORTH_POINTS);
+  gpath_move_to(north, arrow_centre);
 
   Tuplet initial_values[] = {
     TupletCString(DISTANCE_KEY, "NO GPS"),
-    TupletInteger(AZIMUT_INDEX_KEY, 0),
+    TupletInteger(BEARING_INDEX_KEY, 0),
     TupletInteger(EXTRAS_KEY, 0),
     TupletCString(DT_RATING_KEY, ""),
     TupletCString(GC_NAME_KEY, ""),
     TupletCString(GC_CODE_KEY, ""),
-    TupletCString(GC_SIZE_KEY, "")
+    TupletCString(GC_SIZE_KEY, ""),
+    TupletInteger(AZIMUTH_KEY, 0),
+    TupletCString(DECLINATION_KEY, "D"),
   };
+
+  const int inbound_size = 150;
+  const int outbound_size = 0;
+  app_message_open(inbound_size, outbound_size);
 
   app_sync_init(&sync, sync_buffer, sizeof(sync_buffer), initial_values,
                 ARRAY_LENGTH(initial_values), sync_tuple_changed_callback,
@@ -267,14 +381,28 @@ void handle_init(void) {
   // Subscribe to notifications
   bluetooth_connection_service_subscribe(bluetooth_connection_changed);
   tick_timer_service_subscribe(MINUTE_UNIT, handle_minute_tick);
+// compass service added
+  compass_service_subscribe(handle_compass);
+  compass_service_set_heading_filter(2 * (TRIG_MAX_ANGLE/360));
 
   window_set_click_config_provider(window, config_buttons_provider);
 
 }
 
 void handle_deinit(void) {
+// added unsubscribes
   bluetooth_connection_service_unsubscribe();
   tick_timer_service_unsubscribe();
+  compass_service_unsubscribe();
+
+// added layer destroys
+  text_layer_destroy(text_time_layer);
+  layer_destroy(line_layer);
+  text_layer_destroy(text_distance_layer);
+  gpath_destroy(arrow);
+  layer_destroy(arrow_layer);
+  window_destroy(window);
+
 }
 
 int main(void) {
